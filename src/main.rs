f96f553c79acde043967e8ba5d856c448358356e
zwenderk <zwenderk@hotmail.com>
2019-04-02 14:17:35 +0200

    Video 2 - 3:47:53

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
-// --------------------------------------------------------------------------
-//
-// Rusty Kong
-// Copyright (C) 2018 Jeff Panici
-// All rights reserved.
-//
-// This software source file is licensed according to the
-// MIT License.  Refer to the LICENSE file distributed along
-// with this source file to learn more.
-//
-// --------------------------------------------------------------------------
-
 extern crate sdl2;
 
-use std::env;
-use std::vec::Vec;
-use std::path::Path;
+use sdl2::event::Event;
+use sdl2::image::{INIT_PNG, LoadTexture};
+use sdl2::keyboard::Keycode;
 use sdl2::rect::Rect;
 use sdl2::render::TextureCreator;
-use sdl2::image::{LoadTexture, INIT_PNG};
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use std::env;
+use std::path::Path;
+use std::vec::Vec;
 
 #[allow(dead_code)]
 enum ToolMode {
     Palette,
     Tile,
-    Sprite
+    Sprite,
 }
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
@@ -36,13 +24,13 @@ struct Palette {
     entries: [PaletteEntry; 4]
 }
 
-fn run(mode:ToolMode, png: &Path) {
+fn run(mode: ToolMode, png: &Path) {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     struct WindowSize(u32, u32);
     let window_size = match mode {
-        ToolMode::Palette   => WindowSize(800,  600),
+        ToolMode::Palette => WindowSize(800, 600),
         ToolMode::Tile => WindowSize(1151, 17),
         ToolMode::Sprite => WindowSize(1155, 33)
     };
@@ -68,14 +56,14 @@ fn run(mode:ToolMode, png: &Path) {
 
     match mode {
         ToolMode::Palette => {
-            let mut palettes = [Palette {entries: [PaletteEntry(0, 0, 0); 4]}; 64];
-            let mut current_palette: Palette = Palette {entries: [PaletteEntry(0, 0, 0); 4]};
+            let mut palettes = [Palette { entries: [PaletteEntry(0, 0, 0); 4] }; 64];
+            let mut current_palette: Palette = Palette { entries: [PaletteEntry(0, 0, 0); 4] };
 
-            let mut palette_idx:usize = 0;
-            let mut palette_entry_idx = 0;
+            let mut palette_idx: usize = 0;
+            let mut palette_entry_idx: usize = 0;
 
-            let mut _cy = 2;
-            let mut _cx = 2;
+            let mut _cy: usize = 2;
+            let mut _cx: usize = 2;
 
             for _y in 0..16 {
                 for _x in 0..16 {
@@ -86,7 +74,9 @@ fn run(mode:ToolMode, png: &Path) {
                     }
 
                     let pixels = canvas
-                        .read_pixels(Rect::new(_cx, _cy, 1, 1), canvas.default_pixel_format())
+                        .read_pixels(
+                            Rect::new(_cx as i32, _cy as i32, 1, 1),
+                            canvas.default_pixel_format())
                         .unwrap();
                     current_palette.entries[palette_entry_idx] = PaletteEntry(pixels[0], pixels[1], pixels[2]);
                     palette_entry_idx += 1;
@@ -99,43 +89,45 @@ fn run(mode:ToolMode, png: &Path) {
             }
 
             println!("lazy_static! {{");
-            println!("\tstatic ref PAL_CNTL:Vec<Palette> = vec!([");
+            println!("\tstatic ref PAL_CNTL:Vec<Palette> = vec![");
             let mut palette_number = 0;
             for palette in palettes.iter() {
                 println!("\t\t// #{}", palette_number);
                 println!("\t\tPalette {{");
-                println!("\t\t\tentries: vec!([");
+                println!("\t\t\tentries: [");
                 for entry in palette.entries.iter() {
                     println!(
-                        "\t\t\t\tPaletteEntry {{r: 0x{:02x}, g: 0x{:02x}, b: 0x{:02x}}},",
+                        "\t\t\t\tPaletteEntry {{r: 0x{:02x}, g: 0x{:02x}, b: 0x{:02x}, a: 0xff}},",
                         entry.0,
                         entry.1,
                         entry.2);
                 }
-                println!("\t\t\t])");
+                println!("\t\t\t]");
                 println!("\t\t}},");
                 palette_number += 1;
             }
-            println!("\t]);");
+            println!("\t];");
             println!("}}");
         }
 
         ToolMode::Tile => {
             let mut _cy = 0;
             let mut _cx = 0;
-            let mut match_palette: Palette = Palette {entries: [
-                PaletteEntry(0x00, 0x00, 0x00),
-                PaletteEntry(0x94, 0x31, 0xec),
-                PaletteEntry(0x06, 0x04, 0x8e),
-                PaletteEntry(0xff, 0xf3, 0x14),
-            ]};
+            let mut match_palette: Palette = Palette {
+                entries: [
+                    PaletteEntry(0x00, 0x00, 0x00),
+                    PaletteEntry(0x94, 0x31, 0xec),
+                    PaletteEntry(0x06, 0x04, 0x8e),
+                    PaletteEntry(0xff, 0xf3, 0x14),
+                ]
+            };
 
-            let mut tiles = [[0 as u8; 8*8]; 256];
+            let mut tiles = [[0 as u8; 8 * 8]; 256];
             let mut tile_idx = 0;
 
             for _ty in 0..2 {
                 for _tx in 0..128 {
-                    let mut tile_data = [0 as u8; 8*8];
+                    let mut tile_data = [0 as u8; 8 * 8];
                     for _y in 0..8 as usize {
                         for _x in 0..8 as usize {
                             let pixels = canvas
@@ -146,8 +138,8 @@ fn run(mode:ToolMode, png: &Path) {
                             let mut entry_idx = 0;
                             for entry in match_palette.entries.iter() {
                                 if entry.0 == pixels[0]
-                                && entry.1 == pixels[1]
-                                && entry.2 == pixels[2] {
+                                    && entry.1 == pixels[1]
+                                    && entry.2 == pixels[2] {
                                     tile_data[_y * 8 + _x] = entry_idx;
                                     break;
                                 }
@@ -163,12 +155,12 @@ fn run(mode:ToolMode, png: &Path) {
                 _cy += 9;
             }
 
-            println!("lazy_static! {{");
-            println!("\tstatic ref TILE_BITMAPS = [");
+
+            println!("static TILE_BITMAPS: [[u8; TILE_WIDTH*TILE_HEIGHT]; TILE_MAX] = [");
             let mut tile_number = 0;
             for tile_data in tiles.iter() {
-                println!("\t\t// tile #{}", tile_number);
-                print!("\t\t[");
+                println!("\t// tile #{}", tile_number);
+                print!("\t[");
                 for _y in 0..8 as usize {
                     for _x in 0..8 as usize {
                         if _x > 0 {
@@ -177,27 +169,28 @@ fn run(mode:ToolMode, png: &Path) {
                         print!("0x{:02x}", tile_data[_y * 8 + _x]);
                     }
                     if _y < 7 {
-                        print!("\n\t\t ");
+                        print!(",\n\t ");
                     }
                 }
                 println!("],\n");
                 tile_number += 1;
             }
             println!("];");
-            println!("}}");
         }
 
         ToolMode::Sprite => {
             let mut _cy = 0;
             let mut _cx = 0;
-            let mut match_palette: Palette = Palette {entries: [
-                PaletteEntry( 0x00, 0x00, 0x00),
-                PaletteEntry( 0xff, 0x9d, 0x9e),
-                PaletteEntry( 0xff, 0xf3, 0x14),
-                PaletteEntry( 0x0a, 0x07, 0xe8),
-            ]};
+            let mut match_palette: Palette = Palette {
+                entries: [
+                    PaletteEntry(0x00, 0x00, 0x00),
+                    PaletteEntry(0xff, 0x9d, 0x9e),
+                    PaletteEntry(0xff, 0xf3, 0x14),
+                    PaletteEntry(0x0a, 0x07, 0xe8),
+                ]
+            };
 
-            let mut sprites = [[0 as u8; 16*16]; 128];
+            let mut sprites = [[0 as u8; 16 * 16]; 128];
             let mut sprite_idx = 0;
 
             for _ty in 0..2 {
@@ -205,7 +198,7 @@ fn run(mode:ToolMode, png: &Path) {
                     if sprite_idx == 96 {
                         break;
                     }
-                    let mut spr_data = [0 as u8; 16*16];
+                    let mut spr_data = [0 as u8; 16 * 16];
                     for _y in 0..16 as usize {
                         for _x in 0..16 as usize {
                             let pixels = canvas
@@ -233,12 +226,12 @@ fn run(mode:ToolMode, png: &Path) {
                 _cy += 17;
             }
 
-            println!("lazy_static! {{");
-            println!("\tstatic ref SPRITE_BITMAPS = [");
+            //println!("lazy_static! {{");
+            println!("static SPRITE_BITMAPS: [[u8; SPRITE_WIDTH*SPRITE_HEIGHT]; SPRITE_MAX] = [");
             let mut sprite_number = 0;
             for spr_data in sprites.iter() {
-                println!("\t\t// sprite #{}", sprite_number);
-                print!("\t\t[");
+                println!("\t// sprite #{}", sprite_number);
+                print!("\t[");
                 for _y in 0..16 as usize {
                     for _x in 0..16 as usize {
                         if _x > 0 {
@@ -246,29 +239,29 @@ fn run(mode:ToolMode, png: &Path) {
                         }
                         print!("0x{:02x}", spr_data[_y * 16 + _x]);
                     }
+
                     if _y < 15 {
-                        print!("\n\t\t ");
+                        print!(",\n\t ");
                     }
                 }
                 println!("],\n");
                 sprite_number += 1;
             }
             println!("];");
-            println!("}}");
+            //println!("}}");
         }
     }
 
     'mainloop: loop {
         for event in sdl_context.event_pump().unwrap().poll_iter() {
             match event {
-                Event::Quit{..} |
-                Event::KeyDown {keycode: Option::Some(Keycode::Escape), ..} =>
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Option::Some(Keycode::Escape), .. } =>
                     break 'mainloop,
                 _ => {}
             }
         }
     }
-
 }
 
 fn main() {
@@ -278,10 +271,10 @@ fn main() {
         println!("Usage: cargo run pal|tile|sprite /path/to/image.png")
     } else {
         let mode = match args[1].as_ref() {
-            "pal"     => ToolMode::Palette,
-            "tile"    => ToolMode::Tile,
-            "sprite"  => ToolMode::Sprite,
-            _         => panic!("Unknown mode!")
+            "pal" => ToolMode::Palette,
+            "tile" => ToolMode::Tile,
+            "sprite" => ToolMode::Sprite,
+            _ => panic!("Unknown mode!")
         };
 
         run(mode, Path::new(&args[2]));
